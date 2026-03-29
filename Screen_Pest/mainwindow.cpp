cb53575294aae5d4fdf66b57803eb59e2d3341ed
Damian <ciaszczd@gmail.com>
2026-03-29 14:44:57 +0200

    no.1. switch case apologetic

--- Screen_Pest/mainwindow.cpp
+++ Screen_Pest/mainwindow.cpp
@@ -1,9 +1,22 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 #include <iostream>
 #include <memory>
 #include <QGuiApplication>
 #include <QByteArray>
+
+enum class Platform {
+    Wayland,
+    Xcb,
+    Windows,
+    Unknown
+};
+
+static const QHash<QString, Platform> platformMap = {
+    {"wayland", Platform::Wayland},
+    {"xcb",     Platform::Xcb},
+    {"windows", Platform::Windows}
+};
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,20 +32,20 @@ MainWindow::~MainWindow()
 
 
 std::unique_ptr<FloatingWindow> FloatingWindowFactory() {
-    QString platform = QGuiApplication::platformName();
+    Platform p = platformMap.value(QGuiApplication::platformName(), Platform::Unknown);
 
-    if(platform == "wayland"){
-        if (qEnvironmentVariableIsSet("HYPRLAND_INSTANCE_SIGNATURE")){
-            return std::make_unique<HyprLayerShellHybrid>();
-        }
-        else if(qEnvironmentVariableIsSet("KDE_FULL_SESSION")){
-            return std::make_unique<KWinLayerShellHybrid>();
-        }
-        return nullptr;
+    switch (p){
+    case Platform::Wayland:
+            if (qEnvironmentVariableIsSet("HYPRLAND_INSTANCE_SIGNATURE"))
+                return std::make_unique<HyprLayerShellHybrid>();
+            else if(qEnvironmentVariableIsSet("KDE_FULL_SESSION"))
+                return std::make_unique<KWinLayerShellHybrid>();
+            return nullptr;
+        case Platform::Xcb:
+            return std::make_unique<X11Window>();
+        case Platform::Windows:
+            return std::make_unique<WindowsWindow>();
+        default:
+            return nullptr;
     }
-    else if(platform == "xcb")
-        return std::make_unique<X11Window>();
-    else if(platform == "windows")
-        return std::make_unique<WindowsWindow>();
-    return nullptr;
 }
